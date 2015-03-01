97f06adeaff306aaff2512299b788006188d9fb3
Simon O <saouderkirk@gmail.com>
2015-03-01 12:40:27 -0500

    Three Choices
    Added choices, structure for all choices

--- LittlesLaw.py
+++ LittlesLaw.py
@@ -1,10 +1,26 @@
 arrivalRate = 0
 timeSpent = 0
+avgCustomers = 0
 
-print("What is the average arrival rate per hour?")
-arrivalRate = int(raw_input())
+# setup choice, looking for average number, arrival rate, or average time in system
 
-print("What is the average time, in minutes, that a customer stays in the system?")
-timeSpent = int(raw_input())
+print("Are you trying to find:")
+print("1: The long term average number of customers in a stable system")
+print("2: The long term average effective arrival rate")
+print("or")
+print("3: The average time a customer spends in the system?")
 
-print("Your average number of customers within the system is " + str(int(arrivalRate * (timeSpent*0.0166))))
+selection = int(raw_input())
+
+if selection == 1:
+	print("What is the average arrival rate per hour?")
+	arrivalRate = int(raw_input())
+	print("What is the average time, in minutes, that a customer stays in the system?")
+	timeSpent = int(raw_input())
+	print("Your average number of customers within the system is " + str(int(arrivalRate * (timeSpent*0.0166))))
+elif selection == 2:
+	print("You selected " + str(selection))
+elif selection == 3:
+	print("You selected " + str(selection))
+else:
+	print("You selected " + str(selection) + ", please select 1, 2 or 3.")
